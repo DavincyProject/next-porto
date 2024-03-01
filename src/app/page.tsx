1e8ef5a45ee896466f7db4bbe81c5c7078074d38
DavincyProject <bdavincy.bd@gmail.com>
2024-03-01 15:33:40 +0700

    feat: add footer:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="min-h-dvh">tes</div>
+    </>
+  );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Github, Instagram } from "lucide-react";
 import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 import {
   Tooltip,
@@ -8,8 +13,16 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import Image from "next/image";
 
 const Footer = () => {
+  const [email, setEmail] = useState<string>("");
+
+  const sendEmailConnect = (e: any) => {
+    e.preventDefault();
+    setEmail(email);
+  };
+
   const icon = [
     {
       id: 1,
@@ -27,8 +40,41 @@ const Footer = () => {
 
   return (
     <footer className="flex flex-col ">
-      <div>
-        <div className="px-16">a</div>
+      <div className="bg-[#F6F5F5]">
+        <div className="px-16 py-2 flex justify-between items-start">
+          <div className="flex items-center gap-2 font-semibold">
+            <Image
+              className="rounded-full shadow-md border"
+              src={"/logo.png"}
+              alt="logo"
+              width={50}
+              height={50}
+            />
+            Davincy Project
+          </div>
+          <div>
+            <h1 className="font-semibold">About</h1>
+            <ul>
+              <li>testing 1</li>
+              <li>testing 2</li>
+              <li>testing 3</li>
+            </ul>
+          </div>
+          <div>
+            <h1 className="font-semibold">Let&apos;s Connect</h1>
+            <form onClick={sendEmailConnect}>
+              <div className="flex w-full max-w-sm items-center space-x-2">
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Button type="submit">Send</Button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
       <div className="bg-cream">
         <div className="px-16 py-6 flex justify-between items-center">
